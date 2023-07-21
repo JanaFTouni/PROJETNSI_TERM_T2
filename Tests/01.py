d12deb44da88c0492ff4ccaee7ff695cccc25256
JanaFTouni <104241980+JanaFTouni@users.noreply.github.com>
2023-07-21 18:40:25 -0300

    Update 01.py

--- Tests/01.py
+++ Tests/01.py
@@ -1,22 +1,27 @@
 import pygame
+import sys
 
 #constantes du jue
+
 TITLE = "Nuquinha"
 WIDTH = 800
 HEIGHT = 600
-LOGO = 'media/billiard-ball.png'
+LOGO = 'billiard-ball.png'
 
 #initialisateur
+
 pygame.init()
 pygame.display.set_caption(TITLE)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 logo = pygame.image.load(LOGO)
 pygame.display.set_icon(logo)
 
-#Update 
-running = True 
-while running:
-    for event in pygame.event.get() :
-        if event.type == pygame.QUIT(): # verifier si on a cliker quitter 
-            running = False 
- 
+#Update
+
+is_running = True
+while is_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
