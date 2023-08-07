c3fb2e2c7d1a82642f84d0faf52f2e572a695ca2
Joé <99928569+ManoPixel@users.noreply.github.com>
2023-08-07 20:48:22 -0300

    Create 01.py

---
+++ Tests - Mario/01.py
@@ -0,0 +1,13 @@
+import pyxel
+
+class App:
+    def __init__(self):
+         pyxel.init(256, 240, title="01 - Mario")
+         
+         pyxel.run(self.update, self.draw)
+         
+    def update(self):
+        
+    def draw(self):
+        
+App()
