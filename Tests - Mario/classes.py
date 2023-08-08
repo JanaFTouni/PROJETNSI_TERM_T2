55e71c3ff992900baeefbb48327124e8ad198122
Joé <99928569+ManoPixel@users.noreply.github.com>
2023-08-07 21:09:20 -0300

    Create classes.py

---
+++ Tests - Mario/classes.py
@@ -0,0 +1,11 @@
+import pyxel
+
+class Player:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+        
+        self.longueur = 13
+        self.taille = 16
+        self.u = 0
+        self.v = 0
