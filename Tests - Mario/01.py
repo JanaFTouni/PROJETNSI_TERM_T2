916d94d54fe9256074cbb841da561375ce9e4293
Joé <99928569+ManoPixel@users.noreply.github.com>
2023-08-07 21:08:59 -0300

    Update 01.py

--- Tests - Mario/01.py
+++ Tests - Mario/01.py
@@ -1,13 +1,23 @@
 import pyxel
+import classes
 
 class App:
     def __init__(self):
          pyxel.init(256, 240, title="01 - Mario")
-         
+         self.joueur = classes.Player(0, 0)
          pyxel.run(self.update, self.draw)
          
     def update(self):
-        
+        self.joueur.x = 100
+        self.joueur.y = 100
+    
     def draw(self):
+        pyxel.load("my_resource.pyxres", True, True, True, True)
+        
+        pyxel.cls(0)
+        
+        pyxel.blt(self.joueur.x, self.joueur.y,
+                  0, self.joueur.u, self.joueur.v,
+                  self.joueur.longueur, self.joueur.taille, 0)
         
 App()
