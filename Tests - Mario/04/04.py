0ea9d2ffe1c9c8ecc349ae8886c3601731d1433c
Joé <99928569+ManoPixel@users.noreply.github.com>
2023-08-08 20:56:00 -0300

    Create 04.py

---
+++ Tests - Mario/04/04.py
@@ -0,0 +1,51 @@
+import pyxel
+import classes
+
+def collision_generale(blocs, joueur):
+    recul = 0
+    for bloc in blocs:
+        temp = bloc.collision(joueur)
+        if abs(temp) > 0:
+            recul = temp
+            
+    return recul
+
+def mouvement_general(blocs, recul):
+    for bloc in blocs:
+        if pyxel.btn(pyxel.KEY_D):
+            bloc.x -= 1
+            
+        elif pyxel.btn(pyxel.KEY_A):
+            bloc.x += 1
+        bloc.x += recul
+      
+class App:
+    def __init__(self):
+         pyxel.init(256, 240, title="04 - Mouvement :)")
+         self.joueur = classes.Player(100, 100)
+         self.blocs = [classes.Bloc(16 * i, 128, 16, 0) for i in range(16)]
+         self.blocs += [classes.Bloc(16*3, 112, 16, 0)]
+         self.blocs += [classes.Bloc(16 * i, 80, 16, 0) for i in range(16)]
+         self.recul = 0
+         pyxel.run(self.update, self.draw)
+         
+    def update(self):    
+        self.joueur.mouvement()
+        
+        print(self.recul)
+        mouvement_general(self.blocs, self.recul)
+        self.recul = collision_generale(self.blocs, self.joueur)
+    
+    def draw(self):
+        pyxel.load("my_resource.pyxres", True, True, True, True)
+        
+        pyxel.bltm(0, 0, 0, 0, 0, 256, 240)
+        
+        pyxel.blt(self.joueur.x, self.joueur.y,
+                  0, self.joueur.u, self.joueur.v,
+                  self.joueur.longueur, self.joueur.taille, 0)
+        
+        for bloc in self.blocs:
+            pyxel.blt(bloc.x, bloc.y, 0, bloc.u, bloc.v, 16, 16, 0)
+        
+App()
