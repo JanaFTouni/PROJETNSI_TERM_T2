0466bbb16e7b9626c47687fff9c45ce08597e4b9
Joé <99928569+ManoPixel@users.noreply.github.com>
2023-08-07 21:39:59 -0300

    Update 01.py

--- Tests - Mario/01.py
+++ Tests - Mario/01.py
@@ -4,12 +4,15 @@ import classes
 class App:
     def __init__(self):
          pyxel.init(256, 240, title="01 - Mario")
-         self.joueur = classes.Player(0, 0)
+         self.joueur = classes.Player(100, 100)
+         self.blocs = [classes.Bloc(16 * i, 128, 16, 0) for i in range(16)]
          pyxel.run(self.update, self.draw)
          
     def update(self):
-        self.joueur.x = 100
-        self.joueur.y = 100
+        self.joueur.mouvement()
+        
+        for bloc in self.blocs:
+            bloc.collision_sup(self.joueur)
     
     def draw(self):
         pyxel.load("my_resource.pyxres", True, True, True, True)
@@ -19,5 +22,8 @@ class App:
         pyxel.blt(self.joueur.x, self.joueur.y,
                   0, self.joueur.u, self.joueur.v,
                   self.joueur.longueur, self.joueur.taille, 0)
+        
+        for bloc in self.blocs:
+            pyxel.blt(bloc.x, bloc.y, 0, bloc.u, bloc.v, 16, 16, 0)
         
 App()
